9ea1a06ce39435c411993bf0908d5eb576896307
nikulshin.dmitriy <dmn.itprof@gmail.com>
2021-04-24 00:40:29 +0300

    edit project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import  './styles.scss'
 
 console.log('work!!!')
+// const check = document.querySelector('.form-fields__checkbox')
+//
+// check.addEventListener('click',function () {
+//     if(check.checked) {
+//         check.style.background = '#EF483E;'
+//     }
+// })
